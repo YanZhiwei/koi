c407287fd7d06fdebfae93cb3fed9c9f21a877dd
YanZhiwei <Yan.Zhiwei@hotmail.com>
2024-09-23 15:15:52 +0800

    移除automations相关

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
-import asyncio
-
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from apis.v1.job import jobRouter
 from apis.v1.rpa import rpaRouter
-from automations.zhipin import Zhipin
-from curd.job_boss_curd import create_job_boss
-from curd.job_curd import create_job, exists_job
 from database.database import engine
 from schemas.base_schema import Base
 from schemas.job_boss_schema import Job_Boss
@@ -33,33 +28,8 @@ async def welcome() -> dict:
     return {"message": "Welcome to my Page"}
 
 
-def main():
-    zhipin = Zhipin("北京")
-    result = zhipin.search("python")
-    count: int = len(result)
-    print(f"Found {count} jobs")
-    add_jobs_count: int = 0
-    exists_job_count: int = 0
-    for job_summary in result:
-        exist = exists_job(job_summary.id)
-        if exist == False:
-            job = zhipin.get_job(job_summary)
-            if job.detail == None or job.detail == "":
-                print(f"Job:{job_summary.id} detail is empty")
-                continue
-            create_job(job)
-            create_job_boss(job)
-            add_jobs_count += 1
-            print(f"Job:{job_summary.id} added")
-        else:
-            print(f"Job:{job_summary.id} already exists")
-            exists_job_count += 1
-    print(f"Added {add_jobs_count} jobs,exist:{exists_job_count} jobs")
-
-
 if __name__ == "__main__":
     Base.metadata.create_all(
         engine, tables=[Job.__table__, Job_Boss.__table__, RpaTask.__table__]
     )
-    # main()
     uvicorn.run(app="main:app", host="127.0.0.1", port=8000, reload=True)

7654ee9f5e72398d4456a8e507e37d1784e5caba
YanZhiwei <Yan.Zhiwei@hotmail.com>
2024-10-16 22:49:38 +0800

    element-plus 按需导入

--- clientApp/src/App.vue
+++ clientApp/src/App.vue
@@ -4,4 +4,13 @@
 
 <template>
   <router-view></router-view>
+  <el-button type="primary" @click="() => $router.push('/')">返回首页</el-button>
 </template>
+
+<style>
+#app {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+</style>
